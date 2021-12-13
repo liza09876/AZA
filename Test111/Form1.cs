05f4042f94586e388a072a123d725c68eb7bd4fc
liza09876 <liza.lypenko123@gmail.com>
2021-12-13 11:26:30 +0300

    Добавьте файлы проекта.

---
+++ Test111/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test111
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 1, y = 99;
+            int g = Convert.ToInt32(id.Text);
+            if (g < x)
+            {
+                MessageBox.Show("значение не может быть меньше 1 или больше 99", "Ошибка");
+            }
+            else if (g > y)
+            {
+                MessageBox.Show("значение не может быть меньше 1 или больше 99", "Ошибка");
+            }
+            else
+            {
+                Form2 s = new Form2();
+                s.Show();
+                s.Owner = this;
+            }
+            
+        }
+    }
+}
